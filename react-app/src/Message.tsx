ce2cd5238e8b7d1817126edc6c9c00be9671fad9
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-12 18:33:00 -0500

    test components and installed bootstrap
    test components which the root folder is app and its child is message . installed bootstrap into package json

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,11 @@
 
-import Message from './Message';
+
 //PascalCasing function casing upp
 function Message(){
     //JSX:javascript XML (this code will get compiled down to java script)
-    return <div><Message/></div>
+    const name='Carl'
+    if(name)
+        return <h1>hello {name}</h1>;//you can also call a function like 'getName()'
+    return <h1>Hello World </h1>;
 }
+export default Message
