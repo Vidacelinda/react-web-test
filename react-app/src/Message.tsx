300e22f421044db0a26241a5f746013f6a08597a
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-12 17:45:21 -0500

    set up react v2
    set up react vite and run to local host

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+
+import Message from './Message';
+//PascalCasing function casing upp
+function Message(){
+    //JSX:javascript XML (this code will get compiled down to java script)
+    return <div><Message/></div>
+}
