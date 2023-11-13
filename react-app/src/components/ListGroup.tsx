fb9180d67330f38fb48c2fd36fd07f19451fe12f
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-13 11:06:48 -0500

    fixing function
    if function fix and easy

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,16 +2,15 @@ function ListGroup() {
   //array /note let is a varable and can be reassigned  ,const is a constant , duhhhh lmao
   let items = ["New York", "San Diago", "Dubi", "Tokyo"];
   items = [];
-  if (items.length === 0) return <p>No item found </p>;
   //array has a method to map items of a difrent type.
   //convert each item to an li elenent dynamicly
   items.map((item) => <li>{item}</li>);
-  // Note cant use "className" word becauase its a reserved key word in javascript or type script
+  const message = items.length === 0 ? <p>No item Found </p> : null;
   //JSX markup
   return (
     <>
       <h1>List</h1>
-
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           // react needs key property to identify each item. for the instant we can use "item" itself
