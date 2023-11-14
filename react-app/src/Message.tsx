6c06488f2534d168b44209a10071705086aacec7
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-14 11:28:33 -0500

    add onClick to each item and show on console

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -5,7 +5,7 @@ function Message(){
     //JSX:javascript XML (this code will get compiled down to java script)
     const name='Carl'
     if(name)
-        return <h1>hello {name}</h1>;//you can also call a function like 'getName()'
-    return <h1>Hello World </h1>;
+        return <h1>Hello {name}</h1>;//you can also call a function like 'getName()'
+    return <h1>Test1 Message component </h1>;
 }
 export default Message
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,21 +1,22 @@
 function ListGroup() {
   //array /note let is a varable and can be reassigned  ,const is a constant , duhhhh lmao
   let items = ["New York", "San Diago", "Dubi", "Tokyo"];
-  items = [];
+//   items = [];
   //array has a method to map items of a difrent type.
   //convert each item to an li elenent dynamicly
   items.map((item) => <li>{item}</li>);
-  const message = items.length === 0 ? <p>No item Found </p> : null;
   //JSX markup
   return (
     <>
       <h1>List</h1>
-      {message}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           // react needs key property to identify each item. for the instant we can use "item" itself
           // when using an API or real world aplication each item has a propety key like "id" so example (item.id)
-          <li key={item}> {item}</li>
+          <li className="list-group-item" key={item} onClick={()=>console.log(item)}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
