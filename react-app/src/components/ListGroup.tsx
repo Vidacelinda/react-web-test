8a1af974d68217bc7cfdd80be2ca17308429f43c
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-14 12:14:28 -0500

    added handelClick event function to be used in JXS markup

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,21 +1,24 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   //array /note let is a varable and can be reassigned  ,const is a constant , duhhhh lmao
   let items = ["New York", "San Diago", "Dubi", "Tokyo"];
-//   items = [];
-  //array has a method to map items of a difrent type.
-  //convert each item to an li elenent dynamicly
-  items.map((item) => <li>{item}</li>);
+  const handleClick =(event:MouseEvent) =>console.log(event)
   //JSX markup
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           // react needs key property to identify each item. for the instant we can use "item" itself
           // when using an API or real world aplication each item has a propety key like "id" so example (item.id)
-          <li className="list-group-item" key={item} onClick={()=>console.log(item)}>
-            {item}
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
+            {item} 
           </li>
         ))}
       </ul>
