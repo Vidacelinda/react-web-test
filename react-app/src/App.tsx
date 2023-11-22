a5b3e85ad8fbabc648fae04148482294b3ab7fe8
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-21 22:12:51 -0500

    added button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./components/alert";
+import Button from "./components/button";
 
 //component
 function App() {
@@ -7,7 +8,10 @@ function App() {
       <Alert>
         Hello <span></span>world
       </Alert>
+      <Button color="primary" onClick={()=> console.log('btn 1 clicked')}>My Button</Button>
+      <Button onClick={()=> console.log('btn 2 clicked')}>My Button</Button>
     </div>
+
   );
 }
 //export App compnet so it could be used somewhere else
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props{
+    children: String;
+    color?:String;
+    // if you dont want it to changfe from the colors then use
+    // color?:'primary'|'secondary|'danger';
+    onClick:()=>void;
+} 
+// default scondary
+export const Button = ({children,onClick,color='secondary'}:Props) => {
+  return (
+  <button className={'btn btn-'+color} onClick={onClick}> {children}</button>
+  )
+};
+export default Button;
